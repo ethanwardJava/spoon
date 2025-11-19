714b21e9234049f1d1da0fcbee4da56db5799f8f
alireza <theethanwardhere@gmail.com>
2025-11-19 08:57:05 +0330

    asserj and BDD

--- src/test/java/com/arcade/UserControllerTest.java
+++ src/test/java/com/arcade/UserControllerTest.java
@@ -2,39 +2,35 @@ package com.arcade;
 
 import com.arcade.controller.UserController;
 import com.arcade.entity.User;
-import com.arcade.repository.UserRepository;
 import com.arcade.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 @ExtendWith(MockitoExtension.class)
 class UserControllerTest {
 
 
     @Mock
-    UserRepository userRepository;
-
-    @Mock
     UserService userService;
 
     @InjectMocks
     UserController userController;
-
-
-
 
 
     @Test
@@ -46,6 +42,7 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("find users based on name")
     void getUserByName_ShouldReturnUsers() {
         User john = User.builder().id(1L).name("John").build();
         User johnny = User.builder().id(2L).name("Johnny").build();
@@ -57,13 +54,39 @@ class UserControllerTest {
         List<User> result = userController.findByName("Johnny");
 
         assertAll(
-                () -> assertEquals(2, result.size()),
-                () -> assertTrue(result.stream().anyMatch(u -> u.getName().equals("John"))),
-                () -> assertTrue(result.stream().anyMatch(u -> u.getName().equals("Johnny")))
+                () -> assertNotNull(result, "Null found"),
+                () -> assertEquals(2, result.size(), "Wrong result size"),
+                () -> assertTrue(result.stream().anyMatch(u -> u.getName().equals("John")), "No Match found"),
+                () -> assertTrue(result.stream().anyMatch(u -> u.getName().equals("Johnny")), "No Match found")
         );
 
         verify(userService).findByName("Johnny");
     }
 
+    @Test
+    void getUserBYIdWithAssertThat() {
 
+        User lola = User.builder().id(1L).name("lola").email("lolcaWhoDoesntsellMeat75@gmail.com").role("Butcher").build();
+        when(userService.findById(1L)).thenReturn(lola);
+        User result = userController.findById(1L);
+
+        assertThat(result.getName())
+                .isNotBlank()
+                .isNotNull()
+                .isNotEqualTo("john")
+                .isEqualTo("lola")
+                .contains("lo");
+        verify(userService).findById(anyLong());
+
+    }
+
+    @Test
+    void getUserByIdWithBDD() {
+        User lola = User.builder().id(1L).name("lola").email("lolcaWhoDoesntsellMeat75@gmail.com").role("Butcher").build();
+        given(userService.findById(1L)).willReturn(lola);
+        User result = userController.findById(1L);
+        assertThat(result.getName())
+                .isNotBlank().isEqualTo("lola");
+        verify(userService).findById(anyLong());
+    }
 }
